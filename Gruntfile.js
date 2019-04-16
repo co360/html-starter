dce9317eeb5843d1d4ad3065bbab472c4abd1507
Zemian Deng <zemian.deng@oracle.com>
2019-04-16 10:26:36 -0400

    Add typescript build into grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,19 +7,29 @@
 var path = require('path');
 
 module.exports = function(grunt) {
-
   require('load-grunt-config')(grunt, {
   	configPath: path.join(process.cwd(), 'scripts/grunt/config')
   });
 
   grunt.loadNpmTasks("@oracle/grunt-oraclejet");
 
+  grunt.initConfig({
+    ts: {
+      default : {
+        tsconfig: './tsconfig.json'
+      }
+    }
+  });
+  grunt.loadNpmTasks("grunt-ts");
+
   grunt.registerTask("build", "Public task. Calls oraclejet-build to build the oraclejet application. Can be customized with additional build tasks.", function (buildType) {
-    grunt.task.run([`oraclejet-build:${buildType}`]);
+    grunt.task.run(['ts', `oraclejet-build:${buildType}`]);
   });
 
   grunt.registerTask("serve", "Public task. Calls oraclejet-serve to serve the oraclejet application. Can be customized with additional serve tasks.", function (buildType) {
-    grunt.task.run([`oraclejet-serve:${buildType}`]);
-  }); 
+    grunt.task.run(['ts', `oraclejet-serve:${buildType}`]);
+  });
+
+  grunt.registerTask("default", ["build"]);
 };
 
