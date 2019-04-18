4f3ae74a685c0388f4d61e462797640f52e58fbd
Zemian Deng <zemian.deng@oracle.com>
2019-04-18 16:27:14 -0400

    Move ojet html files so paths can work again

--- src/js/jet-composites/weather-card/weather-card-viewModel.js
+++ src/js/jet-composites/weather-card/weather-card-viewModel.js
@@ -1,7 +1,11 @@
-define(["require", "exports", "knockout", "ojs/ojcontext"], function (require, exports, ko, Context) {
+define(["require", "exports", "knockout", "jquery", "ojs/ojcontext"], function (require, exports, ko, $, Context) {
     "use strict";
-    //TODO: How do we import RequireJS plugin?
-    //import componentStrings = require('ojL10n!./resources/nls/weather-card-strings');
+    var Forecast = /** @class */ (function () {
+        function Forecast() {
+            this.periods = new Array();
+        }
+        return Forecast;
+    }());
     var WeatherCardViewModel = /** @class */ (function () {
         function WeatherCardViewModel(context) {
             var self = this;
@@ -10,16 +14,27 @@ define(["require", "exports", "knockout", "ojs/ojcontext"], function (require, e
             self.busyResolve = busyContext.addBusyState(options);
             self.composite = context.element;
             //Example observable
-            self.messageText = ko.observable('Current Temperature is 89 degree.');
             self.properties = context.properties;
             //self.res = componentStrings['weather-card'];
             // Example for parsing context properties
             // if (context.properties.name) {
             //     parse the context properties here
             // }
+            // Get and setup weather data from API
+            self.weatherForecast = ko.observable(this.createWeatherForecast());
             //Once all startup and async activities have finished, relocate if there are any async activities
             self.busyResolve();
         }
+        WeatherCardViewModel.prototype.createWeatherForecast = function () {
+            var forecast = new Forecast();
+            var api = "https://api.weather.gov/gridpoints/MLB/25,68/forecast";
+            $.getJSON(api, function (data) {
+                data.properties.periods.slice(0, 5).forEach(function (period) {
+                    forecast.periods.push(period);
+                });
+            });
+            return forecast;
+        };
         return WeatherCardViewModel;
     }());
     return WeatherCardViewModel;
--- src/js/jet-composites/weather-card/weather-card-viewModel.ts
+++ src/js/jet-composites/weather-card/weather-card-viewModel.ts
@@ -6,11 +6,22 @@ import Composite = require("ojs/ojcomposite");
 //TODO: How do we import RequireJS plugin?
 //import componentStrings = require('ojL10n!./resources/nls/weather-card-strings');
 
+interface ForecastPeriod {
+	startTime: string;
+	number: number;
+	name: string;
+	temperature: number;
+	windSpeed: string;
+	shortForecast: string;
+}
+class Forecast {
+	periods = new Array<ForecastPeriod>();
+}
 class WeatherCardViewModel {
 	busyResolve: () => void;
 	composite: Element;
-	messageText: KnockoutObservable<string>;
 	properties: Composite.PropertiesType;
+	weatherForecast: KnockoutObservable<Forecast>;
 
 	constructor(context :Composite.ViewModelContext) {
 		
@@ -23,7 +34,6 @@ class WeatherCardViewModel {
         self.composite = context.element;
 
         //Example observable
-        self.messageText = ko.observable('Current Temperature is 89 degree.');
         self.properties = context.properties;
         //self.res = componentStrings['weather-card'];
 
@@ -32,8 +42,23 @@ class WeatherCardViewModel {
         //     parse the context properties here
         // }
 
+		// Get and setup weather data from API
+		self.weatherForecast = ko.observable(this.createWeatherForecast());
+
         //Once all startup and async activities have finished, relocate if there are any async activities
         self.busyResolve();
+	}
+
+	createWeatherForecast() :Forecast {
+		let forecast = new Forecast();
+		let api = "https://api.weather.gov/gridpoints/MLB/25,68/forecast";
+		$.getJSON(api, function(data){
+			data.properties.periods.slice(0, 5).forEach( (period: ForecastPeriod) => {
+				forecast.periods.push(period);
+			});
+
+		});
+		return forecast;
 	}
 }
 export = WeatherCardViewModel
