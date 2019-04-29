ab776c1559f538c101c770bbd283e5a6346d255a
Zemian Deng <zemian.deng@oracle.com>
2019-04-29 13:03:29 -0400

    Update

--- static/js/requirejs-config-ojet.js
+++ static/js/requirejs-config-ojet.js
@@ -8,5 +8,22 @@ requirejs.config({
         'ojtranslations': '../../web/js/libs/oj/v6.2.0/resources',
         'ojs': '../../web/js/libs/oj/v6.2.0/debug',
         'jqueryui-amd': '../../web/js/libs/jquery/jqueryui-amd-1.12.1',
+
+        'hammerjs': '../../web/js/libs/hammer/hammer-2.0.8',
+        'ojdnd': '../../web/js/libs/dnd-polyfill/dnd-polyfill-1.0.0',
+        'text': '../../web/js/libs/require/text',
+        'signals': '../../web/js/libs/js-signals/signals',
+        'customElements': '../../web/js/libs/webcomponents/custom-elements.min',
+        'proj4': '../../web/js/libs/proj4js/dist/proj4-src',
+        'css': '../../web/js/libs/require-css/css',
+        'touchr': '../../web/js/libs/touchr/touchr',
+        'persist': '../../web/js/libs/persist/min',
+    },
+
+    // Shim configurations for modules that do not expose AMD
+    shim: {
+        'jquery': {
+            exports: ['jQuery', '$']
+        }
     }
 });
