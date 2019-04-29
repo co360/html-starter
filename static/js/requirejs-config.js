164eeec64e399fb87e7afd14ef63066b04b27939
Zemian Deng <zemian.deng@oracle.com>
2019-04-29 11:25:45 -0400

    Update

--- static/js/requirejs-config.js
+++ static/js/requirejs-config.js
@@ -1,6 +1,7 @@
 requirejs.config({
     baseUrl: 'js',
     paths: {
-        'jquery': '../../web/js/libs/jquery/jquery-3.3.1'
+        'jquery': '../../web/js/libs/jquery/jquery-3.3.1',
+        'knockout': '../../web/js/libs/knockout/knockout-3.4.2.debug'
     }
 });
