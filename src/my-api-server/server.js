31430d9579cbcb0acbdd0c72161cbce3cfdc8b21
Zemian Deng <zemian.deng@oracle.com>
2019-05-07 21:12:12 -0400

    Update

--- src/my-api-server/server.js
+++ src/my-api-server/server.js
@@ -1,8 +1,14 @@
 var express = require("express");
+var fs = require('fs');
+
 var app = express();
 app.listen(3000, () => {
     app.get("/", (req, res) => {
         res.json("Hello World".split(""));
+    });
+    app.get("/countries", (req, res) => {
+        var countries = fs.readFileSync('static/json-samples/countries.json', 'utf8');
+        res.json(JSON.parse(countries));
     });
     console.log("Server running on port 3000");
 });
