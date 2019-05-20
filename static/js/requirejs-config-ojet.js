24ddedf5cda8fbbe2bb8d358f9bbbd62276acdb9
Zemian Deng <zemian.deng@oracle.com>
2019-05-20 09:22:11 -0400

    Update

--- static/js/requirejs-config-ojet.js
+++ static/js/requirejs-config-ojet.js
@@ -1,28 +1,29 @@
 requirejs.config({
-    baseUrl: 'js',
-    paths: {
-        'jquery': '../../web/js/libs/jquery/jquery-3.3.1',
-        'knockout': '../../web/js/libs/knockout/knockout-3.4.2.debug',
-        'promise': '../../web/js/libs/es6-promise/es6-promise',
-        'ojL10n': '../../web/js/libs/oj/v6.2.0/ojL10n',
-        'ojtranslations': '../../web/js/libs/oj/v6.2.0/resources',
-        'ojs': '../../web/js/libs/oj/v6.2.0/debug',
-        'jqueryui-amd': '../../web/js/libs/jquery/jqueryui-amd-1.12.1',
+        baseUrl: '/ojet-starter/static/js/ojet-app',
+        paths: {
+            'knockout': '/ojet-starter/web/js/libs/knockout/knockout-3.4.2.debug',
+            'jquery': '/ojet-starter/web/js/libs/jquery/jquery-3.3.1',
+            'jqueryui-amd': '/ojet-starter/web/js/libs/jquery/jqueryui-amd-1.12.1',
+            'promise': '/ojet-starter/web/js/libs/es6-promise/es6-promise',
+            'hammerjs': '/ojet-starter/web/js/libs/hammer/hammer-2.0.8',
+            'ojdnd': '/ojet-starter/web/js/libs/dnd-polyfill/dnd-polyfill-1.0.0',
+            'ojs': '/ojet-starter/web/js/libs/oj/v6.2.0/debug',
+            'ojL10n': '/ojet-starter/web/js/libs/oj/v6.2.0/ojL10n',
+            'ojtranslations': '/ojet-starter/web/js/libs/oj/v6.2.0/resources',
+            'text': '/ojet-starter/web/js/libs/require/text',
+            'signals': '/ojet-starter/web/js/libs/js-signals/signals',
+            'customElements': '/ojet-starter/web/js/libs/webcomponents/custom-elements.min',
+            'proj4': '/ojet-starter/web/js/libs/proj4js/dist/proj4-src',
+            'css': '/ojet-starter/web/js/libs/require-css/css',
+            'touchr': '/ojet-starter/web/js/libs/touchr/touchr',
+            'persist': '/ojet-starter/web/js/libs/persist/min'
+        },
 
-        'hammerjs': '../../web/js/libs/hammer/hammer-2.0.8',
-        'ojdnd': '../../web/js/libs/dnd-polyfill/dnd-polyfill-1.0.0',
-        'text': '../../web/js/libs/require/text',
-        'signals': '../../web/js/libs/js-signals/signals',
-        'customElements': '../../web/js/libs/webcomponents/custom-elements.min',
-        'proj4': '../../web/js/libs/proj4js/dist/proj4-src',
-        'css': '../../web/js/libs/require-css/css',
-        'touchr': '../../web/js/libs/touchr/touchr'
-    },
-
-    // Shim configurations for modules that do not expose AMD
-    shim: {
-        'jquery': {
-            exports: ['jQuery', '$']
+        // Shim configurations for modules that do not expose AMD
+        shim: {
+            'jquery': {
+                exports: ['jQuery', '$']
+            }
         }
     }
-});
+);
