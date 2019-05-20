9edbb354922e8230f4b9ef8e22449b0515c8e2d1
Zemian Deng <zemian.deng@oracle.com>
2019-05-20 10:06:21 -0400

    Update

--- static/js/requirejs-config-ojet-min.js
+++ static/js/requirejs-config-ojet-min.js
@@ -1,12 +1,22 @@
 requirejs.config({
-    baseUrl: 'js',
-    paths: {
-        'jquery': '../../web/js/libs/jquery/jquery-3.3.1',
-        'knockout': '../../web/js/libs/knockout/knockout-3.4.2.debug',
-        'promise': '../../web/js/libs/es6-promise/es6-promise',
-        'ojL10n': '../../web/js/libs/oj/v6.2.0/ojL10n',
-        'ojtranslations': '../../web/js/libs/oj/v6.2.0/resources',
-        'ojs': '../../web/js/libs/oj/v6.2.0/debug',
-        'jqueryui-amd': '../../web/js/libs/jquery/jqueryui-amd-1.12.1'
+        baseUrl: '/ojet-starter/static/js/ojet-app',
+        paths: {
+            'knockout': '/ojet-starter/web/js/libs/knockout/knockout-3.4.2.debug',
+            'jquery': '/ojet-starter/web/js/libs/jquery/jquery-3.3.1',
+            'jqueryui-amd': '/ojet-starter/web/js/libs/jquery/jqueryui-amd-1.12.1',
+            'promise': '/ojet-starter/web/js/libs/es6-promise/es6-promise',
+            'ojs': '/ojet-starter/web/js/libs/oj/v6.2.0/debug',
+            'ojL10n': '/ojet-starter/web/js/libs/oj/v6.2.0/ojL10n',
+            'ojtranslations': '/ojet-starter/web/js/libs/oj/v6.2.0/resources',
+            'text': '/ojet-starter/web/js/libs/require/text',
+            'css': '/ojet-starter/web/js/libs/require-css/css'
+        },
+
+        // Shim configurations for modules that do not expose AMD
+        shim: {
+            'jquery': {
+                exports: ['jQuery', '$']
+            }
+        }
     }
-});
+);
--- static/js/requirejs-config.js
+++ static/js/requirejs-config.js
@@ -1,8 +1,8 @@
 requirejs.config({
-    baseUrl: 'js',
+    baseUrl: '/ojet-starter/static/js/',
     paths: {
-        'jquery': '../../web/js/libs/jquery/jquery-3.3.1',
-        'knockout': '../../web/js/libs/knockout/knockout-3.4.2.debug',
-        'text': 'requirejs-plugins/text'
+        'knockout': '/ojet-starter/web/js/libs/knockout/knockout-3.4.2.debug',
+        'jquery': '/ojet-starter/web/js/libs/jquery/jquery-3.3.1',
+        'text': '/ojet-starter/web/js/libs/require/text'
     }
 });
