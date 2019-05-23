26874cea022b101f90d63e2664bbb8a5ac05a77b
Zemian Deng <zemian.deng@oracle.com>
2019-05-23 15:22:08 -0400

    Update

--- static/knockout-learn/js/main.js
+++
@@ -1,8 +0,0 @@
-requirejs.config({
-    baseUrl: '',
-    paths: {
-        'knockout': '../../../web/js/libs/knockout/knockout-3.4.2.debug',
-        'jquery': '../../../web/js/libs/jquery/jquery-3.3.1',
-        'text': '../../../web/js/libs/require/text'
-    }
-});
