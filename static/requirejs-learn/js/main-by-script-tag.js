049eb0273e029dcd6cfa5423d8e2f4fd0bd71675
Zemian Deng <zemian.deng@oracle.com>
2019-05-23 15:38:55 -0400

    Update css position

--- static/requirejs-learn/js/main-by-script-tag.js
+++ static/requirejs-learn/js/main-by-script-tag.js
@@ -1,7 +1,7 @@
 requirejs.config({
     baseUrl: "js",
     paths: {
-        'jquery': '../../../web/js/libs/jquery/jquery-3.3.1',
+        'jquery': '../../../web/js/libs/jquery/jquery-3.3.1XX',
         'text': '../../../web/js/libs/require/text'
     }
 });
