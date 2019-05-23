07a27b4eb613e52066c2168da60d449b722043ce
Zemian Deng <zemian.deng@oracle.com>
2019-05-23 15:58:58 -0400

    Update

--- static/requirejs-learn/js/main.js
+++ static/requirejs-learn/js/main.js
@@ -3,6 +3,10 @@
 // is more clear though.
 requirejs.config({
     baseUrl: "js",
+
+    /*
+    NOTE these paths are in relative to the "baseUrl"!
+    */
     paths: {
         'jquery': '../../../web/js/libs/jquery/jquery-3.3.1',
         'text': '../../../web/js/libs/require/text'
