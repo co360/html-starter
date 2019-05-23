c2ecf11154533db224d82c86047f295e76d823a1
Zemian Deng <zemian.deng@oracle.com>
2019-05-23 15:08:21 -0400

    Update

--- static/misc-lib-demo/js/requirejs-config.js
+++
@@ -1,8 +0,0 @@
-requirejs.config({
-    baseUrl: '/ojet-starter/static/js/',
-    paths: {
-        'knockout': '/ojet-starter/web/js/libs/knockout/knockout-3.4.2.debug',
-        'jquery': '/ojet-starter/web/js/libs/jquery/jquery-3.3.1',
-        'text': '/ojet-starter/web/js/libs/require/text'
-    }
-});
