f514b89d10cb10ba15ace839acb3838d2e1b5fd8
Zemian Deng <zemian.deng@oracle.com>
2019-05-25 13:57:02 -0400

    Update

---
+++ static/express-learn/static.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Express looks up the files relative to the static directory,
+// so the name of the static directory is not part of the URL.
+app.use(express.static("public"));
+
+// Setup 2nd directory with a virtual path
+app.use('/files', express.static("files"));
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
