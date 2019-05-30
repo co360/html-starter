2bd1c388b38831a30cec514ec84469c738bd9e33
Zemian Deng <zemian.deng@oracle.com>
2019-05-30 11:32:40 -0400

    Update

--- static/ojet-learn/js/config.js
+++ static/ojet-learn/js/config.js
@@ -2,7 +2,7 @@ requirejs.config({
         baseUrl: 'js',
         paths: {
             'knockout': '../../../web/js/libs/knockout/knockout-3.4.2.debug',
-            'jquery': '../../../web/js/libs/jquery/jquery-3.3.1',
+            'jquery': '../../../web/js/libs/jquery/jquery-3.4.1',
             'jqueryui-amd': '../../../web/js/libs/jquery/jqueryui-amd-1.12.1',
             'promise': '../../../web/js/libs/es6-promise/es6-promise',
             'hammerjs': '../../../web/js/libs/hammer/hammer-2.0.8',
