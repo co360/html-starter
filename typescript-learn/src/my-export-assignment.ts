b4405bab16bcd5d3244abe9864b887f7218ce627
Zemian Deng <zemian.deng@oracle.com>
2019-06-12 16:05:54 -0400

    Update

--- typescript-learn/src/my-export-assignment.ts
+++ typescript-learn/src/my-export-assignment.ts
@@ -7,4 +7,4 @@ class Hello3 {
 
 // You can't use "export =" while this module contains other "export" statement!
 // This only works if you have this were the only statement.
-export = Hello3;
+//export = Hello3;
