d6350b263e4faf9392a18a246e0d578df97447a5
Zemian Deng <zemian.deng@oracle.com>
2019-06-12 16:14:08 -0400

    Update

--- typescript-learn/amd-examples/import-plugin.ts
+++ typescript-learn/amd-examples/import-plugin.ts
@@ -1,3 +1,11 @@
-// TODO: This is not working yet!
-//import * as helloText from './lib1/myplugin!hello.txt';
+// TODO: Neither these methods are working.
+
+// declare module "myplugin!*" {
+//     let resource: {[key: string]: any};
+//     export = resource;
+// }
+// import * as helloText from 'lib1/myplugin!hello.txt';
+
+
+/// <amd-dependency path="lib1/myplugin!hello.txt" name="helloText"/>
 //console.log(helloText);
