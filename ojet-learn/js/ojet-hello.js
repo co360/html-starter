db724935108f2c8667be262b3e31f9f89f2a982a
Zemian Deng <zemian.deng@oracle.com>
2019-06-23 18:11:42 -0400

    Update

--- ojet-learn/js/ojet-hello.js
+++
@@ -1,7 +0,0 @@
-define(['knockout'], function (ko) {
-    function AppViewModel() {
-        let self = this;
-        self.helloMessage = ko.observable("Hello OJET World");
-    }
-    return new AppViewModel();
-});
--- ojet-learn/readme.md
+++
@@ -1,2 +0,0 @@
-This is a static folder setup to explore OJET. You don't need
-to run `ojet build` to preview each html page.
