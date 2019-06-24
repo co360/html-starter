9e0a44485cd0eb12ddd320f1994650c38d2ec5f8
Zemian Deng <zemian.deng@oracle.com>
2019-06-24 15:33:06 -0400

    Update

--- ckeditor-learn/js/main-config.js
+++ ckeditor-learn/js/main-config.js
@@ -2,6 +2,7 @@
 requirejs.config({
     baseUrl: "js",
     paths: {
+        'classic-ckeditor': 'https://cdn.ckeditor.com/ckeditor5/12.2.0/classic/ckeditor',
         'inline-ckeditor': 'https://cdn.ckeditor.com/ckeditor5/12.2.0/inline/ckeditor',
         'jquery': '../../resources/js/libs/jquery/jquery-3.4.1',
         'text': '../../resources/js/libs/require/text'
