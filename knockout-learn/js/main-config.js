d303cc7c8afea4d895dc226f29f17d20fd660a84
Zemian Deng <zemian.deng@oracle.com>
2019-06-29 12:14:23 -0400

    Update

--- knockout-learn/js/main-config.js
+++ knockout-learn/js/main-config.js
@@ -4,6 +4,7 @@ requirejs.config({
         'knockout': '../../resources/js/libs/knockout/knockout-3.5.0.debug',
         'jquery': '../../resources/js/libs/jquery/jquery-3.4.1',
         'text': '../../resources/js/libs/require/text',
-        'css': '../../resources/js/libs/require-css/css'
+        'css': '../../resources/js/libs/require-css/css',
+        'domReady': '../../resources/js/libs/require/domReady'
     }
 });
