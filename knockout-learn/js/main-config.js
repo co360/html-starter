4f4cbe9aae14b93b2f970796c3ed3b2b96da72ac
Zemian Deng <zemian.deng@oracle.com>
2019-06-29 10:15:44 -0400

    Update

--- knockout-learn/js/main-config.js
+++ knockout-learn/js/main-config.js
@@ -1,7 +1,7 @@
 requirejs.config({
     baseUrl: 'js',
     paths: {
-        'knockout': '../../resources/js/libs/knockout/knockout-3.4.2.debug',
+        'knockout': '../../resources/js/libs/knockout/knockout-3.5.0.debug',
         'jquery': '../../resources/js/libs/jquery/jquery-3.4.1',
         'text': '../../resources/js/libs/require/text',
         'css': '../../resources/js/libs/require-css/css'
