a0d48d1d0cdbdedefebded918a01012ddf72b98c
Zemian Deng <zemian.deng@oracle.com>
2019-06-29 11:00:20 -0400

    update

--- requirejs-learn/js/main-config.js
+++ requirejs-learn/js/main-config.js
@@ -3,6 +3,8 @@ requirejs.config({
     baseUrl: "js",
     paths: {
         'jquery': '../../resources/js/libs/jquery/jquery-3.4.1',
-        'text': '../../resources/js/libs/require/text'
+        'text': '../../resources/js/libs/require/text',
+        'css': '../../resources/js/libs/require-css/css',
+        'domReady': '../../resources/js/libs/require/domReady'
     }
 });
