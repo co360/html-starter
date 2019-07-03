47407f95571a016f724d8ee04dd37e8e4ceb9494
Zemian Deng <zemian.deng@oracle.com>
2019-07-03 15:12:08 -0400

    Update

--- nodejs-learn/min-web-webpack/src/index.js
+++ nodejs-learn/min-web-webpack/src/index.js
@@ -1,11 +1,4 @@
-import _ from 'lodash';
-
-function component() {
-  const element = document.createElement('div');
-
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
-}
-
-document.body.appendChild(component());
+var $ = require('jquery');
+$(function() {
+  $("#demo").text("Hello World.");
+});
