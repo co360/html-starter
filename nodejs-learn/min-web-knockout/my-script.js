5ac67c6b84132f347f3f39201cb91c15b278d106
Zemian Deng <zemian.deng@oracle.com>
2019-07-04 11:23:36 -0400

    Update

--- nodejs-learn/min-web-knockout/my-script.js
+++ nodejs-learn/min-web-knockout/my-script.js
@@ -1,2 +1,20 @@
 // NOTE: We are in browser env, so DOM API and all global "window" vars are available
 
+// This script is loaded by index.html, which include the following libraries
+// - RequireJS, Knockout and jQuery. So we are free to use these.
+
+requirejs.config({
+    paths: {
+        'knockout': 'https://cdnjs.cloudflare.com/ajax/libs/knockout/3.5.0/knockout-min',
+        'jquery': 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min'
+    }
+});
+
+require(['jquery', 'knockout'], function($, ko) {
+    $(function() {
+        var viewModel = {
+            helloMessage: ko.observable("Hello World")
+        };
+        ko.applyBindings(viewModel);
+    });
+});
