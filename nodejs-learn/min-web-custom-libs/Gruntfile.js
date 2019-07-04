95dd41ecca518cfd5b7f7b3e71900d9d000267ab
Zemian Deng <zemian.deng@oracle.com>
2019-07-04 09:58:56 -0400

    Update

--- nodejs-learn/min-web-custom-libs/Gruntfile.js
+++ nodejs-learn/min-web-custom-libs/Gruntfile.js
@@ -1,4 +1,4 @@
-// TODO Why it only copies 'jquery.js' and nother deps?
+// TODO Why it only copies 'jquery.js' and not other deps?
 module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
