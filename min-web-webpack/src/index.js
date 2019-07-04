405302687d20a120b65ee1c5f08659a409d1c5bc
Zemian Deng <zemian.deng@oracle.com>
2019-07-04 17:25:53 -0400

    Update

--- min-web-webpack/src/index.js
+++ min-web-webpack/src/index.js
@@ -1,8 +1,12 @@
 // NOTE: This script is written as normal "NodeJS" script, hence we use
-// the require("module") to bring in dependencies. But this script will
 // be packaged by build process that will be used in browser env.
-
+// the require("module") to bring in dependencies. But this script will
 var $ = require('jquery');
+var ko = require("knockout");
+
 $(function() {
-  $("#demo").text("Hello World.");
+  var viewModel = {
+    helloMessage: ko.observable("Hello World")
+  };
+  ko.applyBindings(viewModel);
 });
