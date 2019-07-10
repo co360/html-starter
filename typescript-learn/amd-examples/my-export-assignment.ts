c6b696a8dc7b01ccadc6acdbef45b641606e5a04
Zemian Deng <zemian.deng@oracle.com>
2019-07-10 14:29:59 -0400

    Update

---
+++ typescript-learn/amd-examples/my-export-assignment.ts
@@ -0,0 +1,7 @@
+class Hello3 {
+    message = "World3";
+    greeting() {
+        console.log(this.message);
+    }
+}
+export = Hello3;
--- typescript-learn/src/my-export-assignment.ts
+++ typescript-learn/src/my-export-assignment.ts
@@ -5,6 +5,8 @@ class Hello3 {
     }
 }
 
+// You can do "export =" with ECMAScript target.
+
 // You can't use "export =" while this module contains other "export" statement!
 // This only works if you have this were the only statement.
-//export = Hello3;
+// export = Hello3;
