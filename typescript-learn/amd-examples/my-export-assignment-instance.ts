5d8ef23f57bff4be5a0f1075771e11c4e3e71835
Zemian Deng <zemian.deng@oracle.com>
2019-07-27 10:12:06 -0400

    Update

--- typescript-learn/amd-examples/my-export-assignment-instance.ts
+++ typescript-learn/amd-examples/my-export-assignment-instance.ts
@@ -8,12 +8,16 @@ class HelloAssignInstance {
     }
 }
 
+/*
 // Exporting new instance directly
-// export = new HelloAssignInstance();
+// To use this form, you can enable compiler option: "--esModuleInterop"
+// And user must use "myModule.default" prefix
+export = new HelloAssignInstance();
+*/
 
+// TODO: Is this a bug?
 // Exporting new instance using a indirect variable
 // NOTE: For some reason this form allow user to use this import form:
 //    import * as myInstance from "./my-export-assignment-instance";
 let result = new HelloAssignInstance();
 export = result;
-
