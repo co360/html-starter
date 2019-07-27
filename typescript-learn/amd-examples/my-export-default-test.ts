2c83d8761ccd2617bb9c075eb516c7e9f9038f3d
Zemian Deng <zemian.deng@oracle.com>
2019-07-27 10:18:18 -0400

    Update

--- typescript-learn/amd-examples/my-export-default-test.ts
+++ typescript-learn/amd-examples/my-export-default-test.ts
@@ -1,2 +1,10 @@
-import MyHello from "./my-export-default";
-console.log(MyHello);
+import Hello5 from "./my-export-default";
+console.log(new Hello5());
+
+// This should work the same
+import Hello5b from "./my-export-default-end-of-line";
+console.log(new Hello5b());
+
+// Note that you can rename class with "export default"
+import MyHello5 from "./my-export-default";
+console.log(new MyHello5());
--- typescript-learn/amd-examples/my-export-default.ts
+++ typescript-learn/amd-examples/my-export-default.ts
@@ -5,6 +5,10 @@ The difference between "export" and "export default" are:
 matching when importing for use.
 - "export default" is for single name exported in a file, and the name CAN be
 renamed to any value when importing for use.
+
+NOTE: The generated JS code for "export default" is simply using "default()"
+and not even using the class name.
+
  */
 export default class Hello5 {
     message = "Hello World";
