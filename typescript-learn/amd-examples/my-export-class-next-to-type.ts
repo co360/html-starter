c94acc699222f9b19e47bf783d8a1d656aae13b7
Zemian Deng <zemian.deng@oracle.com>
2019-07-27 10:19:52 -0400

    Update

--- typescript-learn/amd-examples/my-export-class-next-to-type.ts
+++ typescript-learn/amd-examples/my-export-class-next-to-type.ts
@@ -1,5 +1,5 @@
 /*
-This is same as the "my-export.ts" example, except we placing the "export"
+This is same as the "my-export-class-one.ts" example, except we placing the "export"
 on same level as the class instead of placing them all at the end of the file.
  */
 export class Hello4 {
