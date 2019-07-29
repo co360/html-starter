15cdeb36e6f569677b6a69588769534d89308f29
Zemian Deng <zemian.deng@oracle.com>
2019-07-29 10:39:42 -0400

    Update

--- typescript-learn/amd-examples/my-export-assignment-instance-test.ts
+++ typescript-learn/amd-examples/my-export-assignment-instance-test.ts
@@ -6,3 +6,8 @@ console.log(helloAssignInstance);
 // variable!
 import * as helloAssignInstance2 from "./my-export-assignment-instance";
 console.log(helloAssignInstance2);
+
+/* This will not compile!
+// Since we did not export with specific name, we cant use this import form
+import {helloAssignInstance} from "./my-export-assignment-instance";
+*/
--- typescript-learn/amd-examples/my-export-default-test.ts
+++ typescript-learn/amd-examples/my-export-default-test.ts
@@ -8,3 +8,9 @@ console.log(new Hello5b());
 // Note that you can rename class with "export default"
 import MyHello5 from "./my-export-default";
 console.log(new MyHello5());
+
+/* This will not compile!
+// NOTE: You can use "import * as" form for "export default".
+import * as mylib from "./my-export-default";
+console.log(new mylib.Hello5());
+ */
