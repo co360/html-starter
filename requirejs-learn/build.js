f80e2587cc9f87ba5ef469a72d8948384970669b
Zemian Deng <zemian.deng@oracle.com>
2019-08-02 13:58:57 -0400

    Update

---
+++ requirejs-learn/build.js
@@ -0,0 +1,39 @@
+/*
+
+# Generate a optimized JS bundle file that can minimize module code.
+#
+# The "r.js" is actually a executable command! It will come packaged when
+# you run "npm install -g requirejs"
+#
+# Using "r.js" command:
+#   - See https://github.com/requirejs/r.js
+#   - The "-o" option is for optimize
+#   - The command can take RequireJS configuration as options
+
+
+# Example of bundle and minify one file:
+#   - The "name" is the single entry point to the application (eg: main.js)
+r.js -o baseUrl=. name=js/main-config.js out=js/temp-optimized-main.js
+
+# Example of using `build.js`:
+r.js -o build.js
+
+# Generate bundle with out minify
+r.js -o build.js optimize=none
+
+ */
+
+
+/* Example of build.js */
+({
+    baseUrl: "js",
+    paths: {
+        'jquery': '../../resources/js/libs/jquery/jquery-3.4.1',
+        'text': '../../resources/js/libs/require/text',
+        'css': '../../resources/js/libs/require-css/css',
+        'domReady': '../../resources/js/libs/require/domReady'
+    },
+    name: "main",
+    out: "temp-main.js"
+})
+
