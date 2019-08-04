06016c99a6c2d268b10beb6504b0c7f29582976d
Zemian Deng <zemian.deng@oracle.com>
2019-08-04 00:10:54 -0400

    Update

--- grunt-learn/hello/Gruntfile.js
+++ grunt-learn/hello/Gruntfile.js
@@ -3,4 +3,8 @@ module.exports = function(grunt) {
     grunt.initConfig({
     });
 
+    grunt.registerTask('default', 'A hello task.', function() {
+        grunt.log.write('Hello World.').ok();
+    });
+
 };
