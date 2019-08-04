ecc0cc90b5772c2f357fdff16cadce80ebdb2a4f
Zemian Deng <zemian.deng@oracle.com>
2019-08-04 10:42:27 -0400

    Update

---
+++ grunt-learn/grunt-watch-files/Gruntfile.js
@@ -0,0 +1,32 @@
+/*
+What is grunt-contrib-watch?
+
+    Run predefined tasks whenever watched file patterns are added, changed or deleted
+
+We will use the watch plugin to learn more on how grunt specify files patterns
+
+See
+- https://github.com/gruntjs/grunt-contrib-watch
+- https://gruntjs.com/configuring-tasks#files
+ */
+module.exports = function(grunt) {
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.initConfig({
+        watch: {
+            scripts: {
+                files: ['src/**/*.js'],
+                tasks: ['hello'],
+                options: {
+                    spawn: false,
+                },
+            },
+        },
+    });
+
+    grunt.registerTask('hello', 'A hello task.', function() {
+        grunt.log.writeln('Hello World.');
+    });
+
+    grunt.registerTask('default', 'A watch demo', ['watch']);
+};
