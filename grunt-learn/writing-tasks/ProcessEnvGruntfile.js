8fb0509fd3b762293826b021c00028d9ca46ae3f
Zemian Deng <zemian.deng@oracle.com>
2019-08-04 09:02:10 -0400

    Update

--- grunt-learn/writing-tasks/ProcessEnvGruntfile.js
+++ grunt-learn/writing-tasks/ProcessEnvGruntfile.js
@@ -1,5 +1,5 @@
 module.exports = function(grunt) {
     grunt.registerTask('default', 'A hello task.', function() {
-        grunt.log.writeln('Hello World.');
+        grunt.log.writeln('process.env: ', process.env);
     });
 };
