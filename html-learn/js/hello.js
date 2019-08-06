08e3653becb02d7f9e382be068cb3ec8382e318b
Zemian Deng <zemian.deng@oracle.com>
2019-08-06 09:20:54 -0400

    Update

--- html-learn/js/hello.js
+++ html-learn/js/hello.js
@@ -1,1 +1,4 @@
+// Learning JS (Beginner)
+// https://developer.mozilla.org/en-US/docs/Learn/JavaScript/First_steps
+
 console.log("Hello World");
---
+++ html-learn/js/quick-tour.js
@@ -0,0 +1,21 @@
+// Learning JS (Beginner)
+// https://developer.mozilla.org/en-US/docs/Learn/JavaScript/First_steps
+
+console.log("Hello World");
+
+var a = "Hi";
+console.log("var a:", a);
+
+console.log("array: ", [1, 2, 3]);
+console.log("mix array: ", [1, 2, 3, "one", "two", "three"]);
+
+console.log("String split: ", "hello".split(''));
+console.log("String upper: ", "hello".toUpperCase());
+console.log("Max number: ", Number.MAX_VALUE);
+console.log("Random: ", Math.random());
+
+if (Math.random() > .5) {
+    console.log("random bigger than .5 is true");
+} else {
+    console.log("random smaller than .5 is true");
+}
