46a05c4a9dfcbc33d2a23947ab504dc80c8ebbb8
Zemian Deng <zemian.deng@oracle.com>
2019-08-05 21:59:33 -0400

    Update

---
+++ nodejs-learn/dependency-demo/hello-extend.js
@@ -0,0 +1,21 @@
+const _ = require("underscore");
+
+function Person(first, last) {
+    this.firstName = first;
+    this.lastName = last;
+}
+Person.prototype.greeting = function() {
+    return "Hello " + this.firstName + " " + this.lastName;
+};
+
+function test() {
+    let e = new Person("Zemian", "Deng");
+    console.log(e);
+    _.keys(e).forEach((i)=> console.log(i));
+}
+test();
+
+function testExtension() {
+    let Employee = function() {};
+    Employee = _.extend(Employee, Person);
+}
