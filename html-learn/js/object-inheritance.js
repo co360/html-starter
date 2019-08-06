4eb8c402c1d7d8d3a2b9445f7be9969fd52ab50a
Zemian Deng <zemian.deng@oracle.com>
2019-08-06 09:05:06 -0400

    Update

--- html-learn/js/object-inheritance.js
+++ html-learn/js/object-inheritance.js
@@ -1,3 +1,38 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/create
+
+// Shape - superclass
+function Shape() {
+    this.x = 0;
+    this.y = 0;
+}
+
+// superclass method
+Shape.prototype.move = function(x, y) {
+    this.x += x;
+    this.y += y;
+    console.info('Shape moved.');
+};
+
+// Rectangle - subclass
+function Rectangle() {
+    Shape.call(this); // call super constructor.
+}
+
+// subclass extends superclass
+Rectangle.prototype = Object.create(Shape.prototype);
+Rectangle.prototype.constructor = Rectangle; //If you don't set Object.prototype.constructor to Rectangle,
+//it will take prototype.constructor of Shape (parent).
+//To avoid that, we set the prototype.constructor to Rectangle (child).
+
+var rect = new Rectangle();
+
+console.log('Is rect an instance of Rectangle?',
+    rect instanceof Rectangle); // true
+console.log('Is rect an instance of Shape?',
+    rect instanceof Shape); // true
+rect.move(1, 1); // Outputs, 'Shape moved.'
+
+
 // https://stackoverflow.com/questions/15192722/javascript-extending-class
 
 // Below are for JS ES2015
