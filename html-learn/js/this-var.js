16d5b13d2e7d4b1330b7a8e02b23cfa7dfcf2915
Zemian Deng <zemian.deng@oracle.com>
2019-08-06 08:53:35 -0400

    Update

---
+++ html-learn/js/this-var.js
@@ -0,0 +1,57 @@
+'use strict';  // Other mode are 'sloppy mode'
+
+// default "this" is the globalThis object in browser (See this-var.html)
+// empty in NodeJS
+console.log("Global this: ", this);
+
+// "this" also behave differently if you are in strict mode (empty) vs global if in sloppy mode.
+function foo() {
+    console.log("A function this: ", this);
+}
+foo();
+
+// we can bind a object to this
+let binding = {name: "Zemian"};
+let fooNew = foo.bind(binding);
+fooNew();
+// or we simply apply 'this'
+foo.apply(binding);
+
+// constructor function
+function Employee() {
+    this.message = "Hello";
+    console.log("Constructor function this: ", this);
+}
+new Employee();
+
+// Object literal this
+let foo2 = {
+    message: "Hi",
+    greeting: function() {
+        console.log(this.message);
+    }
+};
+foo2.greeting();
+
+// Store and rename this for nested functions
+function Employee2() {
+    let self = this;
+    self.message = "Hello";
+
+    self.greeting = function() {
+        console.log("I am inside a method/function, but accessing class this: ", self);
+    }
+}
+new Employee2().greeting();
+
+
+// The JS fat arrow does not have its own "this", but it will search for
+// the enclosing "this" instead. This means you don't want to use fat arrow
+// in some situation:
+//
+// - When writing object literal
+// - When writing constructor function
+// - When writing event listener
+//
+// When not to use fat arrow
+// https://dmitripavlutin.com/when-not-to-use-arrow-functions-in-javascript/
--- nodejs-learn/dependency-demo/hello-extend.js
+++ nodejs-learn/dependency-demo/hello-extend.js
@@ -1,22 +1,10 @@
 const _ = require("underscore");
 
-function Person(first, last) {
-    this.firstName = first;
-    this.lastName = last;
-}
-Person.prototype.greeting = function() {
-    return "Hello " + this.firstName + " " + this.lastName;
+let parent = {
+    message: "Hello",
+    loMessage: function() { return this.message.toLowerCase() }
 };
-
-function test() {
-    let e = new Person("Zemian", "Deng");
-    console.log(e);
-    _.keys(e).forEach((i)=> console.log(i));
-}
-test();
-
-// TODO: How extends class with underscore?
-function testExtension() {
-    let Employee = function() {};
-    Employee = _.extend(Employee, Person);
-}
+let child = {name: "World"};
+_.extend(child, parent);
+console.log(child.message + " " + child.name);
+console.log(child.loMessage());
