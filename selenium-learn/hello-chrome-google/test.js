3bbd3615a170b3f1cdfaebbea4fc223f2534bf66
Zemian Deng <zemian.deng@oracle.com>
2019-08-07 14:34:49 -0400

    Update

--- selenium-learn/hello-chrome-google/test.js
+++ selenium-learn/hello-chrome-google/test.js
@@ -1,1 +1,30 @@
-// See node_modules/selenium-webdriver/examples
+// https://www.seleniumhq.org/docs/03_webdriver.jsp
+// See more on node_modules/selenium-webdriver/examples
+
+const webdriver = require('selenium-webdriver');
+
+let driver = new webdriver.Builder()
+    .forBrowser('chrome')
+    .build();
+
+driver.get('http://www.google.com');
+
+var element = driver.findElement(webdriver.By.name('q'));
+element.sendKeys('Cheese!');
+element.submit();
+
+driver.getTitle().then(function(title) {
+    console.log('Page title is: ' + title);
+});
+
+driver.wait(function() {
+    return driver.getTitle().then(function(title) {
+        return title.toLowerCase().lastIndexOf('cheese!', 0) === 0;
+    });
+}, 3000);
+
+driver.getTitle().then(function(title) {
+    console.log('Page title is: ' + title);
+});
+
+driver.quit();
