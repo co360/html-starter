dd2d5370792d9ca678301fb3627eef4b93782cf2
Zemian Deng <zemian.deng@oracle.com>
2019-08-16 15:02:22 -0400

    Update

--- typescript-learn/amd-examples/my-export-class-one.ts
+++ typescript-learn/amd-examples/my-export-class-one.ts
@@ -2,9 +2,10 @@
 A normal "export" is used in a module to export one or more named objects.
 The user must import the same exact name to use it.
  */
-export class HelloClass1 {
+class HelloClass1 {
     message = "Hello World";
     greeting() {
         console.log(this.message);
     }
 }
+export {HelloClass1};
--- typescript-learn/amd-examples/my-export-default.ts
+++ typescript-learn/amd-examples/my-export-default.ts
@@ -9,6 +9,9 @@ renamed to any value when importing for use.
 NOTE: The generated JS code for "export default" is simply using "default()"
 and not even using the class name.
 
+// NOTE: TS "export default" is more intend for TS import use only.
+// Plain RequireJS still works, but it will NOT be pretty.
+
  */
 export default class Hello5 {
     message = "Hello World";
