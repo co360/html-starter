ec982c9859fff24192f4d49e95526343c3fc96d5
Zemian Deng <zemian.deng@oracle.com>
2019-08-16 12:56:33 -0400

    Update

--- requirejs-learn/js/exports-demo.js
+++ requirejs-learn/js/exports-demo.js
@@ -1,15 +1,15 @@
 define(["require", "exports", "hello"], function (require, exports) {
     "use strict";
-    function ExportsDemoHello() {
-        this.message = "Hello";
+    function ExportsDemoHello(msg) {
+        this.message = msg;
         this.getMessage = function() {
             return this.message;
         }
     }
 
     exports.ExportsDemoHello = ExportsDemoHello;
-    exports.exportsDemoHello1 = new ExportsDemoHello();
-    exports.exportsDemoHello2 = new ExportsDemoHello();
+    exports.exportsDemoHello1 = new ExportsDemoHello("Hi1");
+    exports.exportsDemoHello2 = new ExportsDemoHello("Hi2");
 
     // NOTE the require() needs the module to be include in the deps in
     // define() first! So this is just another way to assign the module
