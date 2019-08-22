9dc1e8edb60a8b5099b5ad30f65982ea17a9d121
Zemian Deng <zemian.deng@oracle.com>
2019-08-22 16:15:02 -0400

    Update

---
+++ html-learn/js/function-fat-arrow.js
@@ -0,0 +1,23 @@
+var f1 = function() {
+    console.log("Normal function");
+};
+var f2 = function(args) {
+    console.log("Function with args: ", args);
+};
+f1();
+f2("Hello");
+
+var f3 = () => console.log("Fat arrow function");
+var f4 = (args) => console.log("Fat arrow function with args", args);
+f3();
+f4("Hello2");
+
+function myfun(func) {
+    console.log("Invoking func param.");
+    func.apply();
+}
+myfun(() => console.log("Hello"));
+
+// This is not valid syntax. Fat arrow requires "() => statement" form, even
+// with empty argument.
+// var f5 = (=> console.log("Fat arrow function"));
