7797a7aa15f762d329c74f61e54efd67e0e222c9
Zemian Deng <zemian.deng@oracle.com>
2019-08-22 16:40:34 -0400

    Update

--- html-learn/js/function-fat-arrow.js
+++ html-learn/js/function-fat-arrow.js
@@ -12,6 +12,12 @@ var f4 = (args) => console.log("Fat arrow function with args", args);
 f3();
 f4("Hello2");
 
+// NOTE: When there is at one arg, then parenthesis is actually not needed!
+var f4b = args => console.log("Fat arrow function with args", args);
+
+// NOTE: But if you have more than one arguments, then parenthesis is needed again!
+var f4c = (p1, p2) => console.log("Fat arrow function with args", args);
+
 function myfun(func) {
     console.log("Invoking func param.");
     func.apply();
