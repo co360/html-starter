7144e7749ac9144ef5a147eaab6677e618d02863
Zemian Deng <zemian.deng@oracle.com>
2019-09-02 17:28:56 -0400

    Update

--- nodejs-learn/http-requests/hello.js
+++
@@ -1,9 +0,0 @@
-/*
- https://codeburst.io/how-to-make-an-http-request-in-nodejs-http-mechanism-libraries-f25ec990d307
-*/
-
-// Using underscore library functions
-const _ = require("underscore");
-
-// Print what's available in the "_" object.
-_.each(_.keys(_).sort(), k => console.log(k));
