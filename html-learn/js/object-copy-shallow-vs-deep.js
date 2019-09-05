1f32aca4e2d131c73e8ba268a9c74d080c96fded
Zemian Deng <zemian.deng@oracle.com>
2019-09-05 17:24:03 -0400

    Update

--- html-learn/js/object-copy-shallow-vs-deep.js
+++ html-learn/js/object-copy-shallow-vs-deep.js
@@ -1,3 +1,37 @@
+// Object.assign (shallow copy)
+var obj = { a: 1 };
+var copy = Object.assign({}, obj);
+console.log(copy); // { a: 1 }
+
+// More tests
+function test() {
+    'use strict';
+
+    let obj1 = { a: 0 , b: { c: 0}};
+    let obj2 = Object.assign({}, obj1);
+    console.log(JSON.stringify(obj2)); // { a: 0, b: { c: 0}}
+
+    obj1.a = 1;
+    console.log(JSON.stringify(obj1)); // { a: 1, b: { c: 0}}
+    console.log(JSON.stringify(obj2)); // { a: 0, b: { c: 0}}
+
+    obj2.a = 2;
+    console.log(JSON.stringify(obj1)); // { a: 1, b: { c: 0}}
+    console.log(JSON.stringify(obj2)); // { a: 2, b: { c: 0}}
+
+    obj2.b.c = 3;
+    console.log(JSON.stringify(obj1)); // { a: 1, b: { c: 3}}
+    console.log(JSON.stringify(obj2)); // { a: 2, b: { c: 3}}
+
+    // Deep Clone
+    obj1 = { a: 0 , b: { c: 0}};
+    let obj3 = JSON.parse(JSON.stringify(obj1));
+    obj1.a = 4;
+    obj1.b.c = 4;
+    console.log(JSON.stringify(obj3)); // { a: 0, b: { c: 0}}
+}
+test();
+
 /*
 // https://we-are.bookmyshow.com/understanding-deep-and-shallow-copy-in-javascript-13438bad941c
 
