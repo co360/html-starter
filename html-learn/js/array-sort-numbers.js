8cedc75931daa1a6b8197b663a1228e1320a0f1c
Zemian Deng <zemian.deng@oracle.com>
2019-09-08 21:31:04 -0400

    Update

--- html-learn/js/array-sort-numbers.js
+++ html-learn/js/array-sort-numbers.js
@@ -7,12 +7,12 @@
 var months = ['March', 'Jan', 'Feb', 'Dec'];
 months.sort();
 console.log(months);
-// expected output: Array ["Dec", "Feb", "Jan", "March"]
+// expected output: Array ["Dec", "Feb", "Jan", "March"] // NOT SORT IN MONTH VALUE, but ALPHABETICALLY
 
 var array1 = [1, 30, 4, 21, 100000];
 array1.sort();
 console.log(array1);
-// expected output: Array [1, 100000, 21, 30, 4] // NOTE THIS IS NOT WHAT WANT!
+// expected output: Array [1, 100000, 21, 30, 4] // NOTE THIS IS NOT WHAT WE NORMALLY WANT!
 
 // NOTE: JS Array.sort will sort alphabetically only! To sort numerically,
 // you need to write your own custom sort function
