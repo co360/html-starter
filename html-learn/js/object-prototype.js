305bdb990552cc34f8819c74d2459df233fe6a41
Zemian Deng <zemian.deng@oracle.com>
2019-09-19 16:11:44 -0400

    Update

--- html-learn/js/object-prototype.js
+++ html-learn/js/object-prototype.js
@@ -1,39 +1,19 @@
-// https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/Object_prototypes
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/prototype
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/proto
 
-// == JS has many ways to create "objects"
-const o1 = {};
-const o2 = new Object();
-const o3 = Object.create({});
-function Person(){} /* Person => Is called the Function Constructor, and you use it with "new" operator. */
-const o4 = new Person();
-console.log("Empty objects: ", o1, o2, o3, o4);
+console.log("== Object's prototype property");
+console.log("Object.prototype", Object.prototype);
+console.log("Object.prototype.__proto__", Object.prototype.__proto__);
+console.log("Object.__proto__", Object.__proto__);
 
-// == Create non-empty object
-function Person2(firstName, lastName){
-    this.firstName = firstName;
-    this.lastName = lastName;
-}
-const o5 = {firstName: "Zemian", lastName: "Deng"};
-const o6 = new Object(Person2);
-const o7 = Object.create(Person2);
-const o8 = new Person2("Zemian", "Deng");
-console.log("Full objects: ", o5, o6, o7, o8);
+console.log("== object's prototype property");
+var emp = new function () {};
+var object = new Object();
+console.log("object, emp", object, emp);
+console.log("object.prototype, emp.prototype", object.prototype, emp.prototype);
+console.log("object.__proto__, emp.__proto__", object.__proto__, emp.__proto__);
+console.log("'Object.getPrototypeOf(object)'", Object.getPrototypeOf(object));
+console.log("'Object.getPrototypeOf(emp)'", Object.getPrototypeOf(emp));
 
-// == Prototype object and it's chain
-// o4.__proto__ => Person.prototype => Object.prototype
-console.log("Function constructor's prototype is same as object instance: ", Person.prototype == Object.getPrototypeOf(o4));
-console.log("object.__proto__ is same as  Object.getPrototypeOf(object):", o4.__proto__ ==   Object.getPrototypeOf(o4));
-console.log("o4.__proto__: ", o4.__proto__);
-console.log("o4.__proto__.__proto__: ", o4.__proto__.__proto__);
-console.log("o4.__proto__.__proto__.__proto__: ", o4.__proto__.__proto__.__proto__);
-
-// == Overriding object properties will break the chain search!
-Person.prototype.foo = "bar";
-// Note that o4.foo has value even after it was first created, then we add to Person function constructor's prototype.
-console.log("Prototype chain value: ", o4.foo, Person.prototype.foo);
-
-// Now change object
-o4.foo = "bar2";
-const o4b = new Person();
-console.log("Prototype chain value: ", o4.foo, o4b.foo, Person.prototype.foo);
-
+console.log("== prototype compare");
+console.log(object.__proto__ == Object.getPrototypeOf(new Object())); // TODO: Why this one is true?
