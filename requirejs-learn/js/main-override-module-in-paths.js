58bd0f7de0d833abcaace1c845a0c22bc79413a4
Zemian Deng <zemian.deng@oracle.com>
2019-10-17 22:31:37 -0400

    update

--- requirejs-learn/js/main-override-module-in-paths.js
+++ requirejs-learn/js/main-override-module-in-paths.js
@@ -2,7 +2,7 @@ requirejs.config({
     baseUrl: "js",
     paths: {
         'hello': 'mymod1/hello', /* We are overriding the "js/hello.js" */
-        'jquery': '../../../web/js/libs/jquery/jquery-3.3.1',
-        'text': '../../../web/js/libs/require/text'
+        'jquery': '../../resources/node_modules/jquery/dist/jquery',
+        'text': '../../resources/node_modules/requirejs-text/text'
     }
 });
