e44877d0396769bd14693e95f83e51f74ecf68ab
Zemian Deng <zemian.deng@oracle.com>
2020-05-12 11:38:21 -0400

    Update

--- test/karma-learn/karma.conf.js
+++ test/karma-learn/karma.conf.js
@@ -1,7 +1,16 @@
 // karma.conf.js
 module.exports = function(config) {
     config.set({
-        basePath: '..',
-        frameworks: ['jasmine'],
+        basePath: './',
+        frameworks: ['mocha', 'chai'],
+        files: [
+            {pattern: 'src/*.js', included: false, watched: false},
+            {pattern: 'test/*.js', included: false, watched: false}
+        ],
+        plugins: [
+            'karma-chrome-launcher',
+            'karma-mocha',
+            'karma-chai'
+        ]
     });
 };
